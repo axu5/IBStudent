cb2f8fc3ffac1cbe95d5dfd2579eae670e7e0ee0
crystal cabins <crystalcabinss@gmail.com>
2022-02-04 13:14:54 +0100

    rework student class

--- src/IBStudent.java
+++ src/IBStudent.java
@@ -7,84 +7,52 @@
  * @version 04.02.2022
  */
 public class IBStudent {
-  String[] courses;
-  String name;
-  String id;
+  private String name = "";
+  private int predictedGrade = 0;
+  private int[] weeklyMinCAS;
+  private int weekN = 0;
+  private int nWeeks = 33;
 
   IBStudent(String name) {
     this.name = name;
-  }
-
-  IBStudent(String name, String id) {
-    this.name = name;
-    this.id = id;
-  }
-
-  public String[] getCourses() {
-    return courses;
+    this.weeklyMinCAS = new int[nWeeks];
   }
 
   public String getName() {
-    return name;
+    return this.name;
   }
 
-  public String getId() {
-    return id;
+  public int getPredictedGrade() {
+    return this.predictedGrade;
   }
 
-  public void setCourses(String[] courses) {
-    // do not do following code:
-    // this.courses = courses;
-    // instead, use the following code:
-    String[] newCourses = new String[courses.length];
-    for (int i = 0; i < courses.length; ++i)
-      newCourses[i] = courses[i];
-    this.courses = newCourses;
-    // this is because you do not want to copy the memory address but instead want
-    // to make a completely new array and not accidentally mutate the original
-    // array.
+  public void setPredictedGrade(int predictedGrade) {
+    this.predictedGrade = predictedGrade;
   }
 
-  public void setName(String name) {
-    this.name = name;
+  public int[] getWeeklyMinCAS() {
+    return this.weeklyMinCAS;
   }
 
-  public void setId(String id) {
-    this.id = id;
+  public void setWeeklyMinCAS(int[] weeklyMinCAS) {
+    for (int i = 0; i < weeklyMinCAS.length; ++i) {
+      this.weeklyMinCAS[weekN + i] = weeklyMinCAS[i];
+    }
+    weekN += weeklyMinCAS.length;
   }
 
-  public void addCourse(String course) {
-    String[] newCourses = new String[this.courses.length + 1];
-    for (int i = 0; i < newCourses.length; ++i) {
-      if (i < this.courses.length) {
-        newCourses[i] = this.courses[i];
-      } else {
-        newCourses[i] = course;
-      }
+  public boolean onTrack() {
+    int totalMinutes = 0;
+    for (int i = 0; i < this.weeklyMinCAS.length; ++i) {
+      totalMinutes += this.weeklyMinCAS[i];
     }
 
-    this.courses = newCourses;
+    int avgMinutes = totalMinutes / this.weeklyMinCAS.length;
+
+    return this.predictedGrade >= 24 && avgMinutes > 90;
   }
 
-  public void removeCourse(String course) {
-    String[] newCourses = new String[this.courses.length - 1];
-    for (int i = 0; i < newCourses.length; ++i) {
-      if (this.courses[i] != course) {
-        newCourses[i] = this.courses[i];
-      }
-    }
-
-    this.courses = newCourses;
-  }
-
-  public void removeCourse(int index) {
-    String[] newCourses = new String[this.courses.length - 1];
-    for (int i = 0; i < newCourses.length; ++i) {
-      if (i != index) {
-        newCourses[i] = this.courses[i];
-      }
-    }
-
-    this.courses = newCourses;
+  public String toString() {
+    return this.name + " (" + this.predictedGrade + ") on track: " + this.onTrack();
   }
 }
